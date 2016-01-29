aabaf3aabc38a78fc9d96ac1f4074de78a74ce2d
ajianisbest <183556045@qq.com>
2016-01-29 16:20:34 +0800

    重做LIST.PAGE  重封LAYER的AJAX加载模式  在某些时候需要用到IFRAME加载

--- org.tinygroup.ui.layer/src/main/resources/static/org/tinygroup/layer/layer.ajax.js
+++ org.tinygroup.ui.layer/src/main/resources/static/org/tinygroup/layer/layer.ajax.js
@@ -4,8 +4,12 @@ layer.config({
     shift: 5
 });
 $(function() {
-    $("body").on("click","[ly-ajax-url]",function(){
-        var url=$(this).attr("ly-ajax-url");
+    $("body").on("click","[ly-ajax]",function(){
+        var url=$(this).attr("href");
+        var type=$(this).attr("ly-ajax-modal");
+        if(!type){
+            type=1;
+        }
         var title=$(this).attr("ly-ajax-title");
         if(!title){
             title="信息";
@@ -18,15 +22,26 @@ $(function() {
         if(!height){
             height="80%";
         }
-        $.get(url,function(str){
-            layer.closeAll();
+        if(type==1){
+            $.get(url,function(str){
+                layer.closeAll();
+                layer.open({
+                    title:title,
+                    type:1,
+                    content: str,
+                    maxmin:true,
+                    area: [width, height]
+                });
+            });
+        }else{
             layer.open({
                 title:title,
-                type:1,
-                content: str,
+                type:2,
+                content: url,
                 maxmin:true,
                 area: [width, height]
             });
-        });
+        }
+        return false;
     });
 })
