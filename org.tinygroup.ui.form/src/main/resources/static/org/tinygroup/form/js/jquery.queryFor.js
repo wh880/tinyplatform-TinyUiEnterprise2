fde3ca8b70102fce9c592870b21e9de6543a6eaa
ajianisbest <183556045@qq.com>
2016-03-16 13:57:22 +0800

    ueditor案例

--- org.tinygroup.ui.form/src/main/resources/static/org/tinygroup/form/js/jquery.queryFor.js
+++ org.tinygroup.ui.form/src/main/resources/static/org/tinygroup/form/js/jquery.queryFor.js
@@ -11,12 +11,13 @@
         var sel=$(sel);
         var _init = function () {
             var v = window.localStorage.getItem(name);
-            if (v)
+            if (v){
                 items = $.parseJSON(v);
-        }
+            }
+        };
         this.list=function(){
             return items;
-        }
+        };
         var _initSel=function(){
             sel.html('<option value="none">**查询历史**</option>');
             for(var i= 0,l=items.length;i<l;i++){
@@ -24,27 +25,27 @@
                     sel.append($("<option>").attr("value",i).html(items[i].text));
                 }
             }
-        }
+        };
         this.add = function (key, val) {
             items.push({text: key, value: val});
             window.localStorage.setItem(name, JSON.stringify(items));
             _initSel();
         }
         this.getVal=function(key){
-            if(!items) return 0;
+            if(!items){return 0;}
             return items[key]["value"]
-        }
-        this.delete=function(key){
+        };
+        this.del=function(key){
             if(items[key]){
                 delete items[key];
                 window.localStorage.setItem(name, JSON.stringify(items));
                 _initSel();
             }
 
-        }
+        };
         this.clear=function(key){
             window.localStorage.setItem(name, JSON.stringify(items));
-        }
+        };
         _init();
         _initSel();
         return this;
