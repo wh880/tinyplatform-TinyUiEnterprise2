bc9b09e3a6387c95917cd0c5f0d6d53d656581f0
ajianisbest <183556045@qq.com>
2016-03-18 10:31:16 +0800

    增加资源压缩功能 封装资源加载过程

--- org.tinygroup.ui.ueditor/src/main/resources/static/org/tinygroup/ueditor/ueditor.all.js
+++ org.tinygroup.ui.ueditor/src/main/resources/static/org/tinygroup/ueditor/ueditor.all.js
@@ -10083,7 +10083,7 @@ UE.plugins['defaultfilter'] = function () {
                     case 'dd':
                         node.tagName = 'li';
                         break;
-                    case 'li':
+                    /*case 'li':
                         var className = node.getAttr('class');
                         if (!className || !/list\-/.test(className)) {
                             node.setAttr()
@@ -10092,7 +10092,7 @@ UE.plugins['defaultfilter'] = function () {
                         UE.utils.each(tmpNodes, function (n) {
                             node.parentNode.insertAfter(n, node);
                         });
-                        break;
+                        break;*/
                     case 'td':
                     case 'th':
                     case 'caption':
@@ -15110,7 +15110,7 @@ UE.plugins['list'] = function () {
     });
     //进入编辑器的li要套p标签
     me.addInputRule(function(root){
-        utils.each(root.getNodesByTagName('li'),function(li){
+        /*utils.each(root.getNodesByTagName('li'),function(li){
             var tmpP = UE.uNode.createElement('p');
             for(var i= 0,ci;ci=li.children[i];){
                 if(ci.type == 'text' || dtd.p[ci.tagName]){
@@ -15140,7 +15140,7 @@ UE.plugins['list'] = function () {
             if(lastChild && lastChild.type == 'text' && /^\s*$/.test(lastChild.data)){
                 p.removeChild(lastChild)
             }
-        });
+        });*/
         if(me.options.autoTransWordToList){
             var orderlisttype = {
                     'num1':/^\d+\)/,
@@ -15264,9 +15264,9 @@ UE.plugins['list'] = function () {
 
             var style = domUtils.getStyle(node, 'list-style-type');
             style && (node.style.cssText = 'list-style-type:' + style);
-            node.className = utils.trim(node.className.replace(/list-paddingleft-\w+/,'')) + ' list-paddingleft-' + type;
+            //node.className = utils.trim(node.className.replace(/list-paddingleft-\w+/,'')) + ' list-paddingleft-' + type;
             utils.each(domUtils.getElementsByTagName(node,'li'),function(li){
-                li.style.cssText && (li.style.cssText = '');
+                //li.style.cssText && (li.style.cssText = '');
                 if(!li.firstChild){
                     domUtils.remove(li);
                     return;
