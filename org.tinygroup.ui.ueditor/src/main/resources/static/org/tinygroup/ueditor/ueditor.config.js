6a6d76886bb993846a8536a9b5de9f81a3650e8f
ajianisbest <183556045@qq.com>
2016-03-18 09:09:57 +0800

    增加资源压缩功能 封装资源加载过程

--- org.tinygroup.ui.ueditor/src/main/resources/static/org/tinygroup/ueditor/ueditor.config.js
+++ org.tinygroup.ui.ueditor/src/main/resources/static/org/tinygroup/ueditor/ueditor.config.js
@@ -355,6 +355,7 @@
 
         //webAppKey 百度应用的APIkey，每个站长必须首先去百度官网注册一个key后方能正常使用app功能，注册介绍，http://app.baidu.com/static/cms/getapikey.html
         //, webAppKey: ""
+        ,allowDivTransToP:false
     };
 
     function getUEBasePath(docUrl, confUrl) {
