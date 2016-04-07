77c4bf03a6fc4ede477c3ff9c1368b844a2b6da5
ajianisbest <183556045@qq.com>
2016-04-07 17:23:35 +0800

    更改ueditor配置后辍为JS  更改FULLCALEDAR依赖关系

---
+++ org.tinygroup.ui.ueditor/src/main/java/org/tinygroup/UeditorAction.java
@@ -0,0 +1,19 @@
+/*
+package org.tinygroup;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+*/
+/**
+ * Created by ajian on 2016/4/7.
+ *//*
+
+@Controller
+public class UeditorAction {
+
+    @RequestMapping("/")
+    public String getConfig() {
+        return "";
+    }
+}
+*/
